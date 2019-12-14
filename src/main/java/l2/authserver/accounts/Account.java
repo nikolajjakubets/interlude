0412f1c0676d6c6c683a6c37f4cccba76821fb49
jura <yk@finch-melrose.com>
2019-12-15 01:50:20 +0300

    refactoring account

--- src/main/java/l2/authserver/accounts/Account.java
+++ src/main/java/l2/authserver/accounts/Account.java
@@ -3,6 +3,7 @@ package l2.authserver.accounts;
 import l2.authserver.database.L2DatabaseFactory;
 import l2.commons.dbutils.DbUtils;
 import l2.commons.net.utils.NetList;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.CallableStatement;
@@ -10,156 +11,92 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 
 @Slf4j
+@Data
 public class Account {
-    private static final String SQLP_ACCOUNT_LOAD = "{CALL `lip_AccountLoad`(?)}";
-    private static final String SQLP_ACCOUNT_CREATE = "{CALL `lip_AccountCreate`(?, ?)}";
-    private static final String SQLP_ACCOUNT_UPDATE = "{CALL `lip_AccountUpdate`(?, ?, ?, ?, ?, ?, ?)}";
-    private final String login;
-    private String passwordHash;
-    private NetList allowedIpList = new NetList();
-    private int accessLevel;
-    private int banExpire;
-    private String lastIP;
-    private int lastAccess;
-    private int lastServer;
-    private String email;
+  private static final String SQLP_ACCOUNT_LOAD = "{CALL `lip_AccountLoad`(?)}";
+  private static final String SQLP_ACCOUNT_CREATE = "{CALL `lip_AccountCreate`(?, ?)}";
+  private static final String SQLP_ACCOUNT_UPDATE = "{CALL `lip_AccountUpdate`(?, ?, ?, ?, ?, ?, ?)}";
+  private final String login;
+  private String passwordHash;
+  private NetList allowedIpList = new NetList();
+  private int accessLevel;
+  private int banExpire;
+  private String lastIP;
+  private int lastAccess;
+  private int lastServer;
+  private String email;
 
-    public Account(String login) {
-        this.login = login;
+  public Account(String login) {
+    this.login = login;
+  }
+
+  public void restore() {
+    Connection con = null;
+    CallableStatement cstmt = null;
+    ResultSet rset = null;
+
+    try {
+      con = L2DatabaseFactory.getInstance().getConnection();
+      cstmt = con.prepareCall("{CALL `lip_AccountLoad`(?)}");
+      cstmt.setString(1, this.login);
+      rset = cstmt.executeQuery();
+      if (rset.next()) {
+        this.setPasswordHash(rset.getString("password").trim());
+        this.setAccessLevel(rset.getInt("accessLevel"));
+        this.setLastServer(rset.getInt("lastServerId"));
+        this.setLastIP(rset.getString("lastIP"));
+        this.setLastAccess(rset.getInt("lastactive"));
+        this.setEmail(rset.getString("email"));
+      }
+    } catch (Exception e) {
+      log.error("restore: eMessage={}, eClass={}", e.getMessage(), e.getClass());
+    } finally {
+      DbUtils.closeQuietly(con, cstmt, rset);
     }
 
-    public String getLogin() {
-        return this.login;
+  }
+
+  public void save() {
+    Connection con = null;
+    CallableStatement cstmt = null;
+
+    try {
+      con = L2DatabaseFactory.getInstance().getConnection();
+      cstmt = con.prepareCall("{CALL `lip_AccountCreate`(?, ?)}");
+      cstmt.setString(1, this.getLogin());
+      cstmt.setString(2, this.getPasswordHash());
+      cstmt.execute();
+    } catch (Exception e) {
+      log.error("save: eMessage={}, eClass={}", e.getMessage(), e.getClass());
+    } finally {
+      DbUtils.closeQuietly(con, cstmt);
     }
 
-    public String getEmail() {
-        return this.email;
+  }
+
+  public void update() {
+    Connection con = null;
+    CallableStatement cstmt = null;
+
+    try {
+      con = L2DatabaseFactory.getInstance().getConnection();
+      cstmt = con.prepareCall("{CALL `lip_AccountUpdate`(?, ?, ?, ?, ?, ?, ?)}");
+      cstmt.setString(1, this.getLogin());
+      cstmt.setString(2, this.getPasswordHash());
+      cstmt.setInt(3, this.getAccessLevel());
+      cstmt.setInt(4, this.getLastServer());
+      cstmt.setString(5, this.getLastIP());
+      cstmt.setInt(6, this.getLastAccess());
+      cstmt.setString(7, this.getEmail());
+      cstmt.execute();
+    } catch (Exception e) {
+      log.error("update: eMessage={}, eClass={}", e.getMessage(), e.getClass());
+    } finally {
+      DbUtils.closeQuietly(con, cstmt);
     }
+  }
 
-    public void setEmail(String val) {
-        this.email = val;
-    }
-
-    public String getPasswordHash() {
-        return this.passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public boolean isAllowedIP(String ip) {
-        return this.allowedIpList.isEmpty() || this.allowedIpList.isInRange(ip);
-    }
-
-    public int getAccessLevel() {
-        return this.accessLevel;
-    }
-
-    public void setAccessLevel(int accessLevel) {
-        this.accessLevel = accessLevel;
-    }
-
-    public int getBanExpire() {
-        return this.banExpire;
-    }
-
-    public void setBanExpire(int banExpire) {
-        this.banExpire = banExpire;
-    }
-
-    public void setLastIP(String lastIP) {
-        this.lastIP = lastIP;
-    }
-
-    public String getLastIP() {
-        return this.lastIP;
-    }
-
-    public int getLastAccess() {
-        return this.lastAccess;
-    }
-
-    public void setLastAccess(int lastAccess) {
-        this.lastAccess = lastAccess;
-    }
-
-    public int getLastServer() {
-        return this.lastServer;
-    }
-
-    public void setLastServer(int lastServer) {
-        this.lastServer = lastServer;
-    }
-
-    public String toString() {
-        return this.login;
-    }
-
-    public void restore() {
-        Connection con = null;
-        CallableStatement cstmt = null;
-        ResultSet rset = null;
-
-        try {
-            con = L2DatabaseFactory.getInstance().getConnection();
-            cstmt = con.prepareCall("{CALL `lip_AccountLoad`(?)}");
-            cstmt.setString(1, this.login);
-            rset = cstmt.executeQuery();
-            if (rset.next()) {
-                this.setPasswordHash(rset.getString("password").trim());
-                this.setAccessLevel(rset.getInt("accessLevel"));
-                this.setLastServer(rset.getInt("lastServerId"));
-                this.setLastIP(rset.getString("lastIP"));
-                this.setLastAccess(rset.getInt("lastactive"));
-                this.setEmail(rset.getString("email"));
-            }
-        } catch (Exception e) {
-            log.error("restore: eMessage={}, eClass={}", e.getMessage(), e.getClass());
-        } finally {
-            DbUtils.closeQuietly(con, cstmt, rset);
-        }
-
-    }
-
-    public void save() {
-        Connection con = null;
-        CallableStatement cstmt = null;
-
-        try {
-            con = L2DatabaseFactory.getInstance().getConnection();
-            cstmt = con.prepareCall("{CALL `lip_AccountCreate`(?, ?)}");
-            cstmt.setString(1, this.getLogin());
-            cstmt.setString(2, this.getPasswordHash());
-            cstmt.execute();
-        } catch (Exception e) {
-          log.error("save: eMessage={}, eClass={}", e.getMessage(), e.getClass());
-        } finally {
-            DbUtils.closeQuietly(con, cstmt);
-        }
-
-    }
-
-    public void update() {
-        Connection con = null;
-        CallableStatement cstmt = null;
-
-        try {
-            con = L2DatabaseFactory.getInstance().getConnection();
-            cstmt = con.prepareCall("{CALL `lip_AccountUpdate`(?, ?, ?, ?, ?, ?, ?)}");
-            cstmt.setString(1, this.getLogin());
-            cstmt.setString(2, this.getPasswordHash());
-            cstmt.setInt(3, this.getAccessLevel());
-            cstmt.setInt(4, this.getLastServer());
-            cstmt.setString(5, this.getLastIP());
-            cstmt.setInt(6, this.getLastAccess());
-            cstmt.setString(7, this.getEmail());
-            cstmt.execute();
-        } catch (Exception e) {
-          log.error("update: eMessage={}, eClass={}", e.getMessage(), e.getClass());
-        } finally {
-            DbUtils.closeQuietly(con, cstmt);
-        }
-
-    }
+  public boolean isAllowedIP(String ip) {
+    return this.allowedIpList.isEmpty() || this.allowedIpList.isInRange(ip);
+  }
 }
